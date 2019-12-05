72d7e2132f9714c647d376840ddf3a3bac84ea21
Studio J Designs <murrayjames0013@gmail.com>
2019-12-06 08:44:17 +1300

    why

--- styles/js/master.js
+++ styles/js/master.js
@@ -1,119 +1,119 @@
-console.log('direction');
-
-
-function initMap() {
-        var directionsService = new google.maps.DirectionsService;
-        var directionsRenderer = new google.maps.DirectionsRenderer;
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 6,
-          center: {lat: 41.85, lng: -87.65}
-        });
-
-        directionsRenderer.setMap(map);
-
-        document.getElementById('submit').addEventListener('click', function() {
-          calculateAndDisplayRoute(directionsService, directionsRenderer);
-        });
-      }
-
-      function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-        var waypts = [];
-        var checkboxArray = document.getElementById('waypoints');
-        for (var i = 0; i < checkboxArray.length; i++) {
-          if (checkboxArray.options[i].selected) {
-            waypts.push({
-              location: checkboxArray[i].value,
-              stopover: true
-            });
-          }
-        }
-
-        directionsService.route({
-          origin: document.getElementById('start').value,
-          destination: document.getElementById('end').value,
-          waypoints: waypts,
-          optimizeWaypoints: true,
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-
-            console.log(response);
-
-            directionsRenderer.setDirections(response);
-            var route = response.routes[0];
-            console.log(route);
-            var summaryPanel = document.getElementById('directions-panel');
-            summaryPanel.innerHTML = '';
-            // For each route, display summary information.
-            for (var i = 0; i < route.legs.length; i++) {
-              var routeSegment = i + 1;
-              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-                  '</b><br>';
-              summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-              summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-              summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-              summaryPanel.innerHTML += route.legs[i].duration.text + '<br><br>';
-              var distance = route.legs[i].distance.text;
-            }
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
-
-
-initMap()
-
-
-
-
-// //date calculation with date pickers
-//
-// $("#startDate").datepicker({
-//   dateFormat: 'yy-mm-dd',
-//   changeMonth: true,
-//   minDate: new Date(),
-//   maxDate: '+1y',
-//   onSelect: function(date){
-//
-//       var selectedDate = new Date(date);
-//       var msecsInADay = 86400000;
-//       var stDate = new Date(selectedDate.getTime() + msecsInADay);
-//
-//      //Set Minimum Date of EndDatePicker After Selected Date of StartDatePicker
-//       $("#endDate").datepicker( "option", "minDate", stDate );
-//       var enDate = new Date(selectedDate.getTime() + 15 * msecsInADay);
+// console.log('direction');
 //
 //
-//       $("#endDate").datepicker( "option", "maxDate", enDate );
+// function initMap() {
+//         var directionsService = new google.maps.DirectionsService;
+//         var directionsRenderer = new google.maps.DirectionsRenderer;
 //
-//   }
-// });
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 6,
+//           center: {lat: 41.85, lng: -87.65}
+//         });
 //
-// $("#endDate").datepicker({
-//   dateFormat: 'yy-mm-dd',
-//   changeMonth: true,
-//   minDate: '+1d',
-// });
+//         directionsRenderer.setMap(map);
 //
-// //Find the number of days between dates
-// function dateDiff() {
+//         document.getElementById('submit').addEventListener('click', function() {
+//           calculateAndDisplayRoute(directionsService, directionsRenderer);
+//         });
+//       }
 //
-//   var start = $('#startDate').datepicker('getDate');
-//   var end = $('#endDate').datepicker('getDate');
-//   var days   = (end - start)/1000/60/60/24;
+//       function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+//         var waypts = [];
+//         var checkboxArray = document.getElementById('waypoints');
+//         for (var i = 0; i < checkboxArray.length; i++) {
+//           if (checkboxArray.options[i].selected) {
+//             waypts.push({
+//               location: checkboxArray[i].value,
+//               stopover: true
+//             });
+//           }
+//         }
+//
+//         directionsService.route({
+//           origin: document.getElementById('start').value,
+//           destination: document.getElementById('end').value,
+//           waypoints: waypts,
+//           optimizeWaypoints: true,
+//           travelMode: 'DRIVING'
+//         }, function(response, status) {
+//           if (status === 'OK') {
+//
+//             console.log(response);
+//
+//             directionsRenderer.setDirections(response);
+//             var route = response.routes[0];
+//             console.log(route);
+//             var summaryPanel = document.getElementById('directions-panel');
+//             summaryPanel.innerHTML = '';
+//             // For each route, display summary information.
+//             for (var i = 0; i < route.legs.length; i++) {
+//               var routeSegment = i + 1;
+//               summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+//                   '</b><br>';
+//               summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+//               summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+//               summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+//               summaryPanel.innerHTML += route.legs[i].duration.text + '<br><br>';
+//               var distance = route.legs[i].distance.text;
+//             }
+//           } else {
+//             window.alert('Directions request failed due to ' + status);
+//           }
+//         });
+//       }
 //
 //
-//   document.getElementById("noDays").value = days ;
-//   return;
-// }
-//
-//
-//
-// $('#submit1').click(function(){
-//   dateDiff();
-// });
+// initMap()
+
+
+
+
+//date calculation with date pickers
+
+$("#startDate").datepicker({
+  dateFormat: 'yy-mm-dd',
+  changeMonth: true,
+  minDate: new Date(),
+  maxDate: '+1y',
+  onSelect: function(date){
+
+      var selectedDate = new Date(date);
+      var msecsInADay = 86400000;
+      var stDate = new Date(selectedDate.getTime() + msecsInADay);
+
+     //Set Minimum Date of EndDatePicker After Selected Date of StartDatePicker
+      $("#endDate").datepicker( "option", "minDate", stDate );
+      var enDate = new Date(selectedDate.getTime() + 15 * msecsInADay);
+
+
+      $("#endDate").datepicker( "option", "maxDate", enDate );
+
+  }
+});
+
+$("#endDate").datepicker({
+  dateFormat: 'yy-mm-dd',
+  changeMonth: true,
+  minDate: '+1d',
+});
+
+//Find the number of days between dates
+function dateDiff() {
+
+  var start = $('#startDate').datepicker('getDate');
+  var end = $('#endDate').datepicker('getDate');
+  var days   = (end - start)/1000/60/60/24;
+
+
+  document.getElementById("noDays").value = days ;
+  return;
+}
+
+
+
+$('#submit1').click(function(){
+  dateDiff();
+});
 
 
 
